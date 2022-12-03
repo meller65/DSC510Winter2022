86def19288e9beb185a8e3bba26402b164baf28f
lezieldc <119647759+lezieldc@users.noreply.github.com>
2022-12-02 19:42:27 -0500

    # Bellevue University # DSC 510-T301 # Week 1 | Assignment 1.1 # Author: Leziel Dela Cruz # Date: 2022-12-02

---
+++ DelaCruz_DSC510/WK1_Assignment1.1_Hello World.py
@@ -0,0 +1,10 @@
+# Bellevue University
+# DSC 510-T301
+# Week 1 | Assignment 1.1
+# Author: Leziel Dela Cruz
+# Date: 2022-12-02
+
+print("Hello World")
+
+git init -b main
+git add . && git commit -m "initial commit"
