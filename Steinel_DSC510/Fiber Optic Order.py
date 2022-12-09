6fc56b6f23098cb07ed2ad717c1b814a5b2745c9
MeganSteinel <119597662+MeganSteinel@users.noreply.github.com>
2022-12-08 20:53:22 -0500

    New

--- Steinel_DSC510/Fiber Optic Order.py
+++ Steinel_DSC510/Fiber Optic Order.py
@@ -8,8 +8,8 @@ company_name = input('What is your Companys name?\n ')
 print('Thank you. We are excited to assist you', company_name)
 ft_of_cable = input('How many feet of cable do you need installed?\n ')
 print(ft_of_cable, 'sounds good')
-install_cost = '10'  # price per foot
-total_cost = int(ft_of_cable) * int(install_cost)
+install_cost = .87  # price per foot
+total_cost = int(ft_of_cable) * install_cost
 print('Let me ring you up a receipt.')
 print('Receipt of Purchase')
 print(company_name)
