7be87802ef611c03e714448d46607fb7a729e44a
Bonertz <Brian.Bonertz@tyson.com>
2023-01-04 18:14:57 -0600

    update

--- Bonertz_DSC510/4.1 Program Assignment.py
+++ Bonertz_DSC510/4.1 Program Assignment.py
@@ -7,13 +7,16 @@
 
 print('Hello!')
 name = input('Please Type Your Company Name.')
-print('Welcome', name)   # Combined this phrase into one line
-def calculation(feet, price):
-    total = feet * price
+print('Welcome', name)
+
+
+def calculation(feetofcable, price):    # Added Function
+    total = feetofcable * price
     return total
+
+
 feet = int(input('How many feet of fiber optic cable do you need installed?'))
-# Removed the int (feet) and placed that function in row 12
-print('You Entered', feet, 'feet')   # combined this phrase into one line and changed from "need" to "Stated"
+print('You Entered', feet, 'feet')
 print('Cost Per Foot of Fiber Optic Cable Is')
 if feet > 500:
     costperfoot = float(.50)
@@ -23,9 +26,10 @@ elif feet > 100:
     costperfoot = float(.80)
 else:
     costperfoot = float(.87)
-print("$", "%.2f"%costperfoot)   # Removed spaced in line 20
-# Added format function to convert costperfoot to dollar amount with 2 decimals
-total_cost = calculation(feet, costperfoot)   # Removed Int from "feet" as it is already done above
-print('Total Cost for', name, 'is', '$', ("%.2f"%total_cost))   # Combined this phrase into one line
-# Added format function to convert total cost to dollar amount with 2 decimals
+
+print("$", "%.2f" % costperfoot)
+
+total_cost = calculation(feet, costperfoot)   # called the calculation function
+print('Total Cost for', name, 'is', '$', ("%.2f" % total_cost))
 print('Thank you for your business!')
+# removed spaces
